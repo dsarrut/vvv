ed7da58c83668fed2a85082be5394c69e9771a6d
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2026-06-15 09:11:55 +0200

    resolve pytest config directory dynamically and update gitignore and settings

--- src/vvv/utils.py
+++ src/vvv/utils.py
@@ -117,7 +117,7 @@ def resolve_relative_path(rel_path, base_dir):
 def get_config_dir() -> Path:
     """Gets the user configuration directory in a cross-platform way."""
     if "PYTEST_CURRENT_TEST" in os.environ:
-        workspace_test_config = Path("/Users/dsarrut/src/py/vvv/.pytest_config")
+        workspace_test_config = Path(__file__).resolve().parent.parent.parent / ".pytest_config"
         workspace_test_config.mkdir(exist_ok=True)
         return workspace_test_config
 
